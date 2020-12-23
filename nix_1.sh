c7ed1e12e4115c6c031111de05c6f0747988b356
RohitDhankar <dhankar.rohit@gmail.com>
2020-12-23 21:42:54 +0530

    cat MultiLineBlankText.txt | tr -s '\n'

--- nix_1.sh
+++ nix_1.sh
@@ -4,3 +4,5 @@ echo 'more lines of some dummy strings through stdin to  text file - through cat
 echo ' so as seen above we can - write text from STDIN to a text file - without the - cat ?' > test3.txt
 echo ' squeezing  MultiLineBlankText file ' cat -s MultiLineBlankText.txt
 echo ' another test line of text ....... spaces                 spaces             '
+echo ' remove all blank lines == cat MultiLineBlankText.txt | tr -s '\n' ' cat MultiLineBlankText.txt | tr -s '\n'
+
