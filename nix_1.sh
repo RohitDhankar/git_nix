da09235ace05c7154feb84132a8704646a4b3aad
RohitDhankar <dhankar.rohit@gmail.com>
2020-12-23 20:57:58 +0530

    MultiLineBlankText.txt

--- nix_1.sh
+++ nix_1.sh
@@ -1,4 +1,5 @@
 # cat == concatenate - try not to do any - cat abuse 
 echo 'more lines of some dummy strings through stdin to  text file - through cat ' | cat > test1.txt
 echo 'more lines of some dummy strings through stdin to  text file - through cat ' > test2.txt
-echo ' so as seen above we can - write text from STDIN o a text file - without the - cat ?' > test3.txt
+echo ' so as seen above we can - write text from STDIN to a text file - without the - cat ?' > test3.txt
+echo ' squeezing  MultiLineBlankText file ' cat -s MultiLineBlankText.txt
